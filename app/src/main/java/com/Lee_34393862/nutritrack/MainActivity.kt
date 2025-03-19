a6ba8df26f8aec56b88502804484c22414139640
Hao Yang <hlee0163@student.monash.edu>
2025-03-19 13:44:28 +0800

    Added text to login page

--- app/src/main/java/com/Lee_34393862/nutritrack/MainActivity.kt
+++ app/src/main/java/com/Lee_34393862/nutritrack/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.Lee_34393862.nutritrack.ui.theme.NutritrackTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +32,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             NutritrackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            "Title",
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.ExtraBold,
+                            fontSize = 36.sp
+                        )
+                        DisclaimerText()
+                        Button(
+                            onClick = {}
+                        ) {
+                            Text("Button")
+                        }
+                        Text(
+                            text = "Designed by Lee Hao Yang (34393862)",
+                            style = MaterialTheme.typography.bodyMedium,
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
             }
         }
@@ -31,17 +64,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun DisclaimerText() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "This app provides general health and nutrition information for educational purposes only.",
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Italic
+        )
+        Text(
+            text = "It is not intended as medical advice, diagnosis, or treatment.",
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Italic
+        )
+        Text(
+            text = "Always consult a qualified healthcare professional before making any changes to your diet, exercise, or health regimen.",
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Italic
+        )
+        Text(
+            text = "Use this app at your own risk.",
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Italic
+        )
+        Text(
+            text = "If you’d like to an Accredited Practicing Dietitian (APD), please visit the Monash Nutrition/Dietetics Clinic (discounted rates for students):",
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Italic
+        )
+        Text(
+            text = "https://www.monash.edu/medicine/scs/nutrition/clinics/nutrition",
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Italic
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NutritrackTheme {
-        Greeting("Android")
     }
 }
