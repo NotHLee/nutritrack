56bab54510479ee17a890a0e937965a0e1382642
Hao Yang <hlee0163@student.monash.edu>
2025-05-15 02:14:08 +0800

    Bug fix with csv data to db insertion and refactoring UserRepository.kt to use StateFlow

--- app/src/main/java/com/Lee_34393862/nutritrack/data/repositories/UserRepository.kt
+++ app/src/main/java/com/Lee_34393862/nutritrack/data/repositories/UserRepository.kt
@@ -2,17 +2,29 @@ package com.Lee_34393862.nutritrack.data.repositories
 
 import android.content.Context
 import com.Lee_34393862.nutritrack.data.AppDatabase
+import com.Lee_34393862.nutritrack.data.dao.FoodIntakeDao
 import com.Lee_34393862.nutritrack.data.dao.PatientDao
+import com.Lee_34393862.nutritrack.data.entities.FoodIntake
 import com.Lee_34393862.nutritrack.data.entities.Patient
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.firstOrNull
+
+data class User(
+    val userId: Int,
+    val password: String,
+)
 
 class UserRepository {
 
     var patientDao: PatientDao
-    var currentUser: Patient? = null
+    var foodIntakeDao: FoodIntakeDao
+
+    // hold current userId as state flow
+    val currentUserId = MutableStateFlow<Int?>(null)
 
     constructor(context: Context) {
         patientDao = AppDatabase.getDatabase(context = context).patientDao()
+        foodIntakeDao = AppDatabase.getDatabase(context = context).foodIntakeDao()
     }
 
     suspend fun authenticate(userId: Int, password: String): Result<String> {
@@ -25,16 +37,39 @@ class UserRepository {
             return Result.failure(Exception("User id does not exist"))
         }
 
+        // null password means this account has not been claimed, registration is required first
+        if (patient.password.isEmpty()) {
+            return Result.failure(Exception("User id has not been registered"))
+        }
+
         if (patient.password != password) {
             return Result.failure(Exception("Password does not match"))
         }
 
-        // cache patient as current user if login successful
-        currentUser = patient
         return Result.success("Login successful")
     }
 
+    suspend fun register(userId: Int, password: String): Result<String> {
+
+        // query for patient entity with userId and match password
+        val patient: Patient? = patientDao.getPatientByUserId(userId = userId).firstOrNull()
+
+        // null means user does not exist
+        if (patient == null) {
+            return Result.failure(Exception("User id does not exist"))
+        }
+
+        // non null password means this account has been claimed
+        if (patient.password.isNotEmpty()) {
+            return Result.failure(Exception("User id has been claimed"))
+        }
+
+        patientDao.update(patient.copy(password = password))
+        return Result.success("User id successfully claimed")
+
+    }
+
     fun logout() {
-        currentUser = null
+        currentUserId.value = null
     }
 }
---
+++ app/src/main/java/com/Lee_34393862/nutritrack/data/viewmodel/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.Lee_34393862.nutritrack.data.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel(context: Context) : ViewModel() {
+
+}
